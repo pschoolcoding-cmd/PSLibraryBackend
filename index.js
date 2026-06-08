e1bf89bf895d1ffae34d461ca1516639ac645751
AxmadovSardor <axmadov6greenclass@gmail.com>
2026-06-08 22:32:52 +0500

    more cooparative.2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // ✅ Only frontend domains go here
 const allowedDomains = [
   'http://localhost:5173',
-  'https://pslibrary-demo.netlify.app/'
+  'https://pslibrary-demo.netlify.app'
 ];
 
 const corsOptions = {
