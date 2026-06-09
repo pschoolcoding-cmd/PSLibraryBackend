616412ad1f7c372417036c0623e5f1973951dede
AxmadovSardor <axmadov6greenclass@gmail.com>
2026-06-09 10:37:33 +0500

    test.1

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const Books = new mongoose.Schema({
   name:{ type:String, default: "none", required: true},
-  image: { type:String, default:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgk8abFHYLv0a4p7JHLils4vSMFOjdIv_nyfGOCZfELZBh4F4QMdR0k2EfJ5pemlMdIrDXlGAj3HFGqc8746iAzd9zwDz8IVbaRZitaWt1GQWYwLJLU9odfgM-Dj1r-froD4bjwseX88Tfs/s1600/unknown+book.jpg"},
+  image: { type:String},
   bid: {type: String, unique: true, required: true},
   description:{type: String, default:""},
   author:{ type: String, required: true},
