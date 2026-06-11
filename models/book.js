8217e6c70b9c6df6a3d3fa734da123ecdcfd5a1f
AxmadovSardor <axmadov6greenclass@gmail.com>
2026-06-11 17:49:43 +0500

    faster loading

--- models/book.js
+++ models/book.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const Books = new mongoose.Schema({
-  name:{ type:String, default: "none", required: true},
+  name:{ type:String, default: "none", required: true, index: true},
   image: { type:String},
   bid: {type: String, unique: true, required: true},
   description:{type: String, default:""},
-  author:{ type: String, required: true},
-  genre:{type: Object, default:{}},
+  author:{ type: String, required: true, index: true},
+  genre:{type: mongoose.Schema.Types.Mixed, default:{}, index: true},
   borrowed: {type: String, default:"0"},
   whoadded: {type: String, default:"none"},
   whentoken:{type:String, default:"01/04/0007"}
